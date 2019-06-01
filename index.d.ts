6df2f93aefe6c95e4e2c478d87b60af1051ab476
skt-t1-byungi <tiniwz@gmail.com>
2019-06-02 08:23:29 +0900

    refactor index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,5 @@
-export declare function createBrowserGetter<Launcher extends (opts: any)=> Promise<any> >(
+export declare function createBrowserGetter<Launcher extends (opts: any)=> Promise<any>>(
     puppet: {launch:Launcher},
-    opts?: Parameters<Launcher>[0]
+    opts?: Launcher extends (opts:infer R) => Promise<any> ? R : never
 ) : () => ReturnType<Launcher>
 export default createBrowserGetter;
